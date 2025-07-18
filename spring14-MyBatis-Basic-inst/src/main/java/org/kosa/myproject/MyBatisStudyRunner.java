ae8b3e3ec102499f6d63cfe84cddb24361db99ef
seo-inst <innerpeace.deepi@gmail.com>
2025-07-18 12:32:43 +0900

    feat : mybatis member

--- spring14-MyBatis-Basic-inst/src/main/java/org/kosa/myproject/MyBatisStudyRunner.java
+++ spring14-MyBatis-Basic-inst/src/main/java/org/kosa/myproject/MyBatisStudyRunner.java
@@ -1,5 +1,9 @@
 package org.kosa.myproject;
 
+
+import java.util.List;
+
+import org.kosa.myproject.domain.Member;
 import org.kosa.myproject.mapper.MemberMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,7 +17,33 @@ public class MyBatisStudyRunner implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		int totalCount = memberMapper.getTotalMemberCount();
-		System.out.println("총 회원수:"+totalCount);
+		//int totalCount = memberMapper.getTotalMemberCount();
+		//System.out.println("총 회원수:"+totalCount);
+		
+		// insert test 
+//		Member member = new Member("mybatis","a","이강인","파리");
+//		int registerResult=memberMapper.register(member);
+//		System.out.println("회원 등록 "+registerResult);// 등록 회원수 
+		
+		// select test 
+		// Mapper xml  에서  parameterType="String"  resultType="org.kosa.myproject.domain.Member" 을 명시하면 됨 
+	//	Member member = memberMapper.findMemberById("mybatis");
+	//	System.out.println(member);// 회원정보 toString 이 출력 : 이강인 정보가 출력 
+		
+		// select List test
+		 List<Member> list=memberMapper.findAllMemberList();
+		 for(Member m:list)
+			 System.out.println(m);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- spring14-MyBatis-Basic-inst/src/main/java/org/kosa/myproject/mapper/MemberMapper.java
+++ spring14-MyBatis-Basic-inst/src/main/java/org/kosa/myproject/mapper/MemberMapper.java
@@ -1,8 +1,14 @@
 package org.kosa.myproject.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.kosa.myproject.domain.Member;
 
 @Mapper
 public interface MemberMapper {
 	int getTotalMemberCount();
+	int register(Member member);
+	Member findMemberById(String id);
+	List<Member> findAllMemberList();
 }
